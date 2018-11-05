aae4f489d277c37dd430f81ba2ca01601e8708e9
Ian Usher <ian@codeplay.com>
2018-11-05 15:01:59 +0000

    Adding ability to create new Sand pages

--- sand.py
+++ sand.py
@@ -1,21 +1,40 @@
 from config.config_loader import ConfigLoader
 import sys
 import os
+import click
 
-def main(args):
-    if len(args) <= 1:
-        print("Error: No path to site root")
-        return 1
+PAGE_TEMPLATE = """title: [Add a page title]
+template: [The template used to render]
 
-    site_root = args[1] if os.path.isdir(args[1]) else None
+# Page Header
 
-    sites = ConfigLoader().load(site_root)
+This new markdown page will need to either be added to your site.json for rendering or may be picked up automatically if 
+contained in a folder using wildcard rules to render.
 
-    for site in sites:
-        site.render()
+"""
 
-    return 0
+
+
+@click.command(context_settings={"ignore_unknown_options": True})
+@click.argument('site_json', type=click.Path(exists=True))
+@click.option("--page", nargs=1, default=None, help="Generate a new page in the provided site")
+def main(site_json, page=None):
+    sites = ConfigLoader().load(click.format_filename(site_json))
+
+    if page is None:
+        #Render
+        for site in sites:
+            site.render()
+    else:
+        #Generate a new page
+        if not os.path.exists(page):
+            os.makedirs(os.path.split(page)[0], exist_ok=True)
+            with open(page, "w") as out:
+                out.write(PAGE_TEMPLATE)
+            click.echo("New page %s created" % page)
+        else:
+            click.echo("The path provided for the new page already exists and cannot be created.")
 
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+    main()
