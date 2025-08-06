da63fd2710274b44af9b00200388a9c0cd77a27a
Ian Usher <ian@headwillcollapse.net>
2025-08-06 17:02:11 +0100

    Adding some config options to allow the redefining of the tags used in the extended markdown and to add types

--- sand/plugin/builtins/extended_markdown.py
+++ sand/plugin/builtins/extended_markdown.py
@@ -5,5 +5,6 @@ from .specialmarkdown.CodeBlocks import BoxExtension
 
 class Plugin(SandPlugin):
     def configure(self, site_data, site):
+        config_options = site_data.get("extended_markdown", {})
         # Here we're going to change the way code is rendered
-        BoxExtension().extendMarkdown(site.renderer)
+        BoxExtension(config_options).extendMarkdown(site.renderer)
--- sand/plugin/builtins/specialmarkdown/CodeBlocks.py
+++ sand/plugin/builtins/specialmarkdown/CodeBlocks.py
@@ -5,9 +5,11 @@ from markdown.blockprocessors import BlockProcessor
 from markdown.blockparser import BlockParser
 
 class BoxBlockProcessor(BlockProcessor):
-    RE_FENCE_START = r'^\s*(?P<start>==[=]*)(?P<type>[^ =\n]+)[^ =\n]*(?P<class_names>\.[^=\n]*)'
-    RE_FENCE_END = r'[\n\s]*(?P<end>==[=]*)[\n\s]*$'
-    RE_FENCE_END_SPECIFIC = r'[\n\s]*%s[\n\s]*$'
+    RE_DEFAULT_OPEN = "{%"
+    RE_DEFAULT_CLOSE = "%}"
+    RE_FENCE_START_TEMPLATE = r'^\s*(?P<start>%s )(?P<type>[^\. \n]+)[^\. \n]*(?P<class_names>\.{0,1}[^\n]*)'
+    RE_FENCE_END_TEMPLATE = r'[\n\s]*(?P<end>%s)[\n\s]*$'
+
     KNOWN_TYPES = {
         "as": "aside",
         "ar": "article",
@@ -15,20 +17,37 @@ class BoxBlockProcessor(BlockProcessor):
         "s": "section"
     }
 
-    def __init__(self, parser: BlockParser):
-        super().__init__(parser)
+    def __init__(self, parser: BlockParser, config_options):
+        if "types" in config_options:
+            self.KNOWN_TYPES = self.KNOWN_TYPES.update(config_options.get("types", {}))
+
+        self.open_tag = self.RE_DEFAULT_OPEN
+        self.close_tag = self.RE_DEFAULT_CLOSE
+
+        if "tags" in config_options:
+            tags = config_options.get("tags")
+            self.open_tag = tags.get("open", self.RE_DEFAULT_OPEN)
+            self.close_tag = tags.get("close", self.RE_DEFAULT_CLOSE)
+
+        self.RE_FENCE_START = self.RE_FENCE_START_TEMPLATE % self.open_tag
+        self.RE_FENCE_END = self.RE_FENCE_END_TEMPLATE % self.close_tag
+
+        print(self.RE_FENCE_START, self.RE_FENCE_END)
+
         self.closing_tags = []
         self.levels = []
         self.completed = False
 
+        super().__init__(parser)
+
     def test(self, parent, block):
         return self.is_start(block)[0]
 
-    @staticmethod
-    def render_element(parent, box_type, class_names):
-        element = BoxBlockProcessor.KNOWN_TYPES.get(box_type, "div")
+    def render_element(self, parent, box_type, class_names):
+        element = self.KNOWN_TYPES.get(box_type, "div")
         e = ET.SubElement(parent, element)
-        e.set('class', class_names)
+        if class_names:
+            e.set('class', class_names)
         return e
 
     def render(self, parent, groups):
@@ -43,7 +62,6 @@ class BoxBlockProcessor(BlockProcessor):
     def is_end(self, block):
         end_match = re.match(self.RE_FENCE_END, block)
         return (end_match is not None, end_match)
-
 
     def open_block(self, parent, block, start_match):
         groups = start_match.groupdict()
@@ -62,7 +80,7 @@ class BoxBlockProcessor(BlockProcessor):
         end_group = groups.get("end")
         restored_element = parent
 
-        if self.closing_tags and end_group == self.closing_tags[-1]:
+        if self.closing_tags:
             self.closing_tags = self.closing_tags[:-1]
             self.levels = self.levels[:-1]
 
@@ -85,7 +103,7 @@ class BoxBlockProcessor(BlockProcessor):
                 if continued:
                     self.parser.parseBlocks(parent, continued)
                     continued = []
-                print("Start an element", counter)
+                print("Start an element", counter, start[1].groupdict().get("type"))
                 parent = self.open_block(parent, block, start[1])
 
             elif end[0]:
@@ -113,5 +131,10 @@ class BoxBlockProcessor(BlockProcessor):
 
 
 class BoxExtension(Extension):
+    def __init__(self, config_options):
+        super().__init__()
+        self.config_options = config_options
+        print(self.config_options)
+
     def extendMarkdown(self, md):
-        md.parser.blockprocessors.register(BoxBlockProcessor(md.parser), 'box', 175)
+        md.parser.blockprocessors.register(BoxBlockProcessor(md.parser, self.config_options), 'box', 175)
